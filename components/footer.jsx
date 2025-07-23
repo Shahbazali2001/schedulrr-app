e3fe0cf29b3577e8776cde5ceef3646dfc6ddf52
Shahbazali2001 <shahbazali5097855@gmail.com>
2025-07-24 04:02:11 +0500

    add components

---
+++ components/footer.jsx
@@ -0,0 +1,16 @@
+import React from "react";
+
+const Footer = () => {
+  return (
+    <footer className="bg-blue-100 py-12">
+      <div className="container mx-auto px-4 text-center text-black">
+        <p>
+          Made with ❤️ by{" "}
+          <a href="https://github.com/shahbazali2001">Shahbaz Ali</a>
+        </p>
+      </div>
+    </footer>
+  );
+};
+
+export default Footer;
---
+++ components/header.jsx
@@ -0,0 +1,52 @@
+import React from "react";
+import Link from "next/link";
+import Image from "next/image";
+import { Button } from "./ui/button";
+import { PenBox } from "lucide-react";
+import UserMenu from "./user-menu";
+
+// Clerk Imports
+import {
+  SignInButton,
+  SignUpButton,
+  SignedIn,
+  SignedOut,
+  UserButton,
+} from "@clerk/nextjs";
+
+
+const Header = () => {
+  return (
+    <nav className="mx-auto py-2 px-4 flex justify-between items-cente shadow-md border-b-2">
+      <Link href={"/"} className="flex items-center">
+        <Image
+          src="/logo.png"
+          width={150}
+          height={60}
+          alt="logo"
+          className="h-16 w-auto"
+        />
+      </Link>
+
+      <div className="flex items-center gap-4">
+        <Link href={"/events?create=true"} className="">
+          <Button>
+            <PenBox /> Create Event
+          </Button>
+        </Link>
+
+        <SignedOut>
+          <SignInButton forceRedirectUrl="/dashboard">
+            <Button variant="outline">Login</Button>
+          </SignInButton>
+        </SignedOut>
+
+        <SignedIn>
+          <UserMenu />
+        </SignedIn>
+      </div>
+    </nav>
+  );
+};
+
+export default Header;
